8d80085a651e198aa7183445e5e7596a965dc0b9
Felix Dilly <felix.dilly@gmail.com>
2026-04-09 18:20:39 +0200

    feat: added toKebabCase function

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 
+/**
+ * Converts a string to snake_case.
+ * @param {string} str - The input string.
+ * @returns {string} The snake_case version of the string.
+ */
 function toSnakeCase(str) {
     if (!str) {
         throw new Error("Empty string given");
@@ -10,6 +15,27 @@ function toSnakeCase(str) {
 
     for (let iterator = 1; iterator < tokens.length; iterator++) {
         newString = newString.concat('_', tokens[iterator].toLowerCase());
+    }
+
+    return newString;
+}
+
+/**
+ * Converts a string into kebab-case
+ * @param {string} str - The input string.
+ * @returns {string} The kebab-case version of the string.
+ */
+function toKebabCase(str) {
+    if (!str) {
+        throw new Error("Empty string given");
+    }
+
+    const tokens = str.split(' ');
+
+    let newString = tokens[0].toLowerCase();
+
+    for (let iterator = 1; iterator < tokens.length; iterator++) {
+        newString = newString.concat('-', tokens[iterator].toLowerCase());
     }
 
     return newString;
